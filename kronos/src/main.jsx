262e38b08800959bfd5f13e9115a66a32053c6b2
TANICE-GAWD <2006princesharma@gmail.com>
2026-02-15 19:33:45 +0530

    Adding textures

--- kronos/src/main.jsx
+++ kronos/src/main.jsx
@@ -16,25 +16,20 @@ document.body.appendChild(renderer.domElement)
 
 
 const geometry = new three.BoxGeometry(1,1,1,1)
-const material = new three.LineBasicMaterial({ color : 0x00ff00 });
-const points=[]
-points.push(new three.Vector3(-10,0,0))
-points.push( new three.Vector3( 0, 10, 0 ) );
-points.push( new three.Vector3( 10, 0, 0 ) );
+const material = new three.MeshBasicMaterial({ color : 0x00ff00 });
+
+const cube = new three.Mesh(geometry, material)
+scene.add(cube)
+
+camera.position.z = 3;
 
 
-// const cube = new three.Mesh(geometry, material)
-// scene.add(cube)
+function animate(){
+  cube.rotation.x += 0.1
+  cube.rotation.y += 0.1
+  renderer.render(scene,camera)
 
-// camera.position.z = 5;
-
-
-// function animate(){
-//   cube.rotation.x += 0.01
-//   cube.rotation.y += 0.01
-//   renderer.render(scene,camera)
-
-// }
+}
 
 renderer.setAnimationLoop(animate);
 
