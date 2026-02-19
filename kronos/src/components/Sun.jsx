9900ded6342b1c64c883778fe55324b7a76c9ac8
TANICE-GAWD <2006princesharma@gmail.com>
2026-02-19 22:59:25 +0530

    added planets

--- kronos/src/components/Sun.jsx
+++ kronos/src/components/Sun.jsx
@@ -6,22 +6,31 @@
 // >> put texture in MeshBasicMaterial 
 // >> put geometry find sphereGeometry...put args
 // >> useRef for meshRef
-
+import * as three from "three"
 import {useLoader, useFrame} from '@react-three/fiber'
 import { TextureLoader } from 'three'
+import { useRef } from "react";
+
 
 
 const Sun = () => {
+
+    const meshRef = useRef()
     const SunTexture = useLoader(
-        TextureLoader(
-            `/textures/sun.jpg`
-        )
+        TextureLoader, 
+            "/textures/sun.jpg"
     )
 
+    useFrame(() => {
+        meshRef.current.rotation.y +=0.005
+    })
+
+
     return(
-        <mesh>
+        <mesh ref={meshRef}>
         <sphereGeometry args = {[1.4,32,32]}/>
         <meshStandardMaterial  map={SunTexture}/>
+
 
         </mesh>
     )
