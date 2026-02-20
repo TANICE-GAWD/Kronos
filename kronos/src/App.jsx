a437de33fdcecc1724b58aef5cc5f67827706fc9
TANICE-GAWD <2006princesharma@gmail.com>
2026-02-20 21:47:56 +0530

    addeing html blackhole

--- kronos/src/App.jsx
+++ kronos/src/App.jsx
@@ -3,6 +3,7 @@ import Sun from "./components/Sun";
 import Planet from "./components/Planet";
 import { Stars, OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
+import blackholeeee from "./blackhole";
 
 function App(){
 
@@ -21,7 +22,7 @@ function App(){
 
       <ambientLight intensity={3}/>
       <directionalLight position={[5, 5, 5]} intensity={1.5} />
-
+      <blackholeeee />
       <Sun/>
 
       {planets.map((planet) => (
---
+++ kronos/src/blackhole/index.jsx
@@ -0,0 +1,24 @@
+import "./style.css";
+
+function blackholeeee(){
+
+  return(
+    <>
+      <div id="controls" class="ui-panel">
+        <div id="autoRotateToggle" class="control-item" title="Toggle automatic rotation"></div>
+        <div id="triggerEffectButton" class="control-item" title="Trigger Disk Echo"></div>
+      </div>
+
+      <div id="theme-changer" class="ui-panel">
+        <div id="theme-buttons">
+          <button class="theme-button active" data-theme="inferno">Inferno</button>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default blackholeeee;
+  <script type="module" src="./script.js"></script>
+
