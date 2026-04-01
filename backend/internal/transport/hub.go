3ca852d7b24a1e33ce817e881f15302ac6cb63da
TANICE-GAWD <2006princesharma@gmail.com>
2026-04-01 16:22:39 +0530

    writing the central hub code for the packets

---
+++ backend/internal/transport/hub.go
@@ -0,0 +1,50 @@
+package transport
+
+import(
+	"time",
+	"net/http",
+	"math/rand",
+	"backend/internal/models/packet"
+
+)
+
+type Hub struct{
+	register chan *Client
+	unregister chan *Client
+	clients map[*Client]bool
+	broadcat chan map[uuid.UUID]packet.Packet
+}
+
+func NewHub () *Hub{
+	return &Hub{
+		clients: make(map[*Client]bool),
+		register: make(chan *Client),
+		unregister: make(chan *Client),
+		broadcast: make(chan map[uuid.UUID]packet.Packet),
+	}
+}
+
+func (h *Hub) RegisterClient(c *Client){
+	h.clients[c] = true;
+}
+
+func (h *Hub) UnRegisterClient (c *Client){
+	_,ok := h.clients[c]
+	if ok{
+		delete(h.clients,c)
+		close(C.send)
+	}
+
+}
+
+func (h *Hub) Run(){
+	select{
+	case client := <- h.register:
+		h.RegisterClient(client)
+	case client := <-h.unregister:
+		h.UnRegisterClient(client)
+	case stateSnapshot := <-h.broadcast:
+		h.BroadCast(stateSnapshot)
+	}
+}
+
